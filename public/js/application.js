44216b3e47e10f3129ed2f3ca0b51382cfc64724
acreilly <acreilly3@gmail.com>
2014-09-07 16:52:04 -0700

    open and close form function works

--- public/js/application.js
+++ public/js/application.js
@@ -2,4 +2,7 @@ $(document).ready(function() {
   $(".emailButton").on("click", function(){
   $(".emailForm").css("display", "block")
 })
+  $("#closeForm").on("click", function(){
+  $(".emailForm").css("display", "none")
+  })
 });
