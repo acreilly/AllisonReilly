fa022d58957485b5451fbfbc8982a45949f29955
acreilly <acreilly3@gmail.com>
2014-09-08 13:50:58 -0700

    Images click and show large, and can click out

--- db/seed.rb
+++
@@ -1,5 +0,0 @@
-counter = 0
-48.times do
-  Image.create(filename: "img" << counter.to_s, category: "photography")
-  counter += 1
-end
--- public/js/application.js
+++ public/js/application.js
@@ -5,4 +5,34 @@ $(document).ready(function() {
   $("#closeForm").on("click", function(){
   $(".emailForm").css("display", "none")
   })
+  portfolioView = new PortfolioView
+  portfolioController = new PortfolioController(portfolioView)
+  portfolioController.initialize()
+
+
 });
+
+
+function PortfolioController(view){
+  this.view = view;
+}
+
+PortfolioController.prototype = {
+  initialize: function(){
+    $("img").on("click", this.showImageContainer)
+    $(".imageContainer").on("click", this.clearImageContainer)
+  },
+  navClick: function(){
+
+  },
+  clearImageContainer: function(){
+      $(".imageContainer").css("display", "none");
+  },
+  showImageContainer: function(){
+    var imgSrc = $(this).attr("src")
+    $(".imageContainer").css("display", "block");
+    $("#singleImage").attr("src", imgSrc)
+  }
+}
+
+function PortfolioView(){}
