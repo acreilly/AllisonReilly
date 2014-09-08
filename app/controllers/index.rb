ae6da879c759746e808a9ff551a914ac4530bb9d
acreilly <acreilly3@gmail.com>
2014-09-07 20:13:01 -0700

    mailer working

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -12,22 +12,23 @@ end
 
 post '/email' do
   options = {
-    address: "smtp.secureserver.net",
-    port: 25,
-    domain: "allisoncreilly.com",
-    user_name: "#{ENV['USERNAME']}",
+    address: "smtp.gmail.com",
+    port: '587',
+    user_name: "#{ENV['GMAIL']}",
     password: "#{ENV['PASSWORD']}",
-    authentication: "plain"
+    authentication: "plain",
+    :enable_starttls_auto => true
   }
+
     email = Mail.new
     email.from = params[:email]
     email.to = "acreilly3@gmail.com"
     email.subject = "#{params[:company]} is trying to contact you"
     email.body = "Name:  #{params[:full_name]} \n Company:  #{params[:company]} \n \n #{params[:message]}"
-binding.pry
 
+    binding.pry
     email.delivery_method :smtp, options
-    if email.deliver
+    if email.deliver!
     redirect "/contact"
   end
 end
