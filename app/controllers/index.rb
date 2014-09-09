87531d099f35ce12f3d54437cf63b893323c4d2f
acreilly <acreilly3@gmail.com>
2014-09-08 18:16:00 -0700

    still testing mail, works on localhost not heroku

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,6 @@
 require 'pry'
+require 'net/smtp'
+
 get '/' do
   erb :index
 end
@@ -12,27 +14,26 @@ get '/contact' do
 end
 
 post '/email' do
-  options = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: "www.allisoncreilly.com",
-    user_name: ENV['GMAIL'],
-    password: ENV['PASSWORD'],
-    authentication: "plain",
-    :enable_starttls_auto => true
-  }
-    email = Mail.new
-    email.from = params[:email]
-    email.to = "acreilly3@gmail.com"
-    email.subject = "#{params[:company]} is trying to contact you"
-    email.body = "Name:  #{params[:full_name]} \n Company:  #{params[:company]} \n \n #{params[:message]}"
+  Mail.defaults do
+    delivery_method :smtp, {
+      :address => 'smtp.gmail.com',
+      :port => '587',
+      :user_name => ENV['GMAIL'],
+      :password => ENV['PASSWORD'],
+      :authentication => :plain,
+      :enable_starttls_auto => true
+    }
+  end
+  email = Mail.new
+  email.from = params[:email]
+  email.to = "acreilly3@gmail.com"
+  email.subject = "#{params[:company]} is trying to contact you"
+  email.body = "Name:  #{params[:full_name]} \n Company:  #{params[:company]} \n \n #{params[:message]}"
 
-    email.delivery_method :smtp, options
-
-    if email.deliver!
+  if email.deliver!
     redirect "/contact"
   else
-redirect '/'
+    redirect '/'
   end
 end
 
