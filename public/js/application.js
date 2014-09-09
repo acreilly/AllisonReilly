f368c80937b8d1865f9a2a3a941c95181d6d9fbf
acreilly <acreilly3@gmail.com>
2014-09-08 21:04:43 -0700

    completed slideshow, added styling

--- public/js/application.js
+++ public/js/application.js
@@ -9,6 +9,16 @@ $(document).ready(function() {
   portfolioController = new PortfolioController(portfolioView)
   portfolioController.initialize()
 
+$("#slideshow > div:gt(0)").hide();
+
+setInterval(function() {
+  $('#slideshow > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshow');
+},  6000);
 
 });
 
