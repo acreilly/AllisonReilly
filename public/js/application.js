b9898c4076e1919b462d35fa8c1003f56afa38eb
acreilly <acreilly3@gmail.com>
2014-09-15 14:47:05 -0400

    changed info

--- public/js/application.js
+++ public/js/application.js
@@ -28,10 +28,10 @@ function PortfolioController(view){
 
 PortfolioController.prototype = {
   initialize: function(){
-    $(".imageThumb img").on("click", this.showImageContainer);
+    $(".main").on("click", "img", this.showImageContainer);
     $(".imageContainer").on("click", this.clearImageContainer);
     $("aside a").on("click", this.navClick);
-    $(".imageContainer").on("click", "#singleImage", this.imageClick);
+    $(".imageContainer").on("click", "#singleImage", this.imageClick)
   },
   navClick: function(e){
     e.preventDefault();
@@ -60,3 +60,66 @@ PortfolioController.prototype = {
     $("#singleImage").attr("alt", imgID)
   }
 }
+
+$(document).ready(function() {
+  $(".emailButton").on("click", function(){
+    $(".emailForm").css("display", "block")
+  })
+  $("#closeForm").on("click", function(){
+    $(".emailForm").css("display", "none")
+  })
+  portfolioController = new PortfolioController
+  portfolioController.initialize()
+
+  $("#slideshow > div:gt(0)").hide();
+
+  setInterval(function() {
+    $('#slideshow > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshow');
+  },  6000);
+
+});
+
+
+// function PortfolioController(view){
+//   this.view = view;
+// }
+
+// PortfolioController.prototype = {
+//   initialize: function(){
+//     $(".imageThumb").on("click", "img", this.showImageContainer);
+//     $(".imageContainer").on("click", this.clearImageContainer);
+//     $("aside a").on("click", this.navClick);
+//     $(".imageContainer").on("click", "#singleImage", this.imageClick);
+//   },
+//   navClick: function(e){
+//     e.preventDefault();
+//     var navLink = $(this)
+//     $.ajax({
+//       method: "get",
+//       url: "/portfolio/" + navLink.attr("id")
+//     }).done(function(){
+//       $("aside a").css("color", "black")
+//       navLink.css("color", "#29AAE1")
+//       $(".main")[0].innerHTML = arguments[0]
+//     })
+//   },
+//   imageClick: function(e){
+//     var image = $(this)
+//     window.location.replace("/portfolio/image/" + image.attr("alt"))
+//   },
+//   clearImageContainer: function(){
+//     $(".imageContainer").css("display", "none");
+//   },
+//   showImageContainer: function(){
+//     var imgSrc = $(this).attr("src")
+//     var imgID = $(this).attr("alt")
+//     $(".imageContainer").css("display", "block");
+//     $("#singleImage").attr("src", imgSrc)
+//     $("#singleImage").attr("alt", imgID)
+//   }
+// }
