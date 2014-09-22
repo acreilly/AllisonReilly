f47285c35b19e23ec8bf470d90ef00e12f56a6dd
acreilly <acreilly3@gmail.com>
2014-09-22 10:40:18 -0400

    fixed links

--- public/js/application.js
+++ public/js/application.js
@@ -61,28 +61,28 @@ PortfolioController.prototype = {
   }
 }
 
-$(document).ready(function() {
-  $(".emailButton").on("click", function(){
-    $(".emailForm").css("display", "block")
-  })
-  $("#closeForm").on("click", function(){
-    $(".emailForm").css("display", "none")
-  })
-  portfolioController = new PortfolioController
-  portfolioController.initialize()
+// $(document).ready(function() {
+//   $(".emailButton").on("click", function(){
+//     $(".emailForm").css("display", "block")
+//   })
+//   $("#closeForm").on("click", function(){
+//     $(".emailForm").css("display", "none")
+//   })
+//   portfolioController = new PortfolioController
+//   portfolioController.initialize()
 
-  $("#slideshow > div:gt(0)").hide();
+//   $("#slideshow > div:gt(0)").hide();
 
-  setInterval(function() {
-    $('#slideshow > div:first')
-    .fadeOut(1000)
-    .next()
-    .fadeIn(1000)
-    .end()
-    .appendTo('#slideshow');
-  },  6000);
+//   setInterval(function() {
+//     $('#slideshow > div:first')
+//     .fadeOut(1000)
+//     .next()
+//     .fadeIn(1000)
+//     .end()
+//     .appendTo('#slideshow');
+//   },  6000);
 
-});
+// });
 
 
 // function PortfolioController(view){
